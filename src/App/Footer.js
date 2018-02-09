c4e1e2097b03c8f824314dec993b1bf4eeb6c3e2
Mark Redeman <markredeman@gmail.com>
2018-02-09 10:00:21 +0100

    Add status to footer, change fake member data

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+const Status = () => (
+  <div>
+    <strong>Status:</strong> connected
+  </div>
+)
+
 const Footer = () => (
     <footer>
         <nav>
           <Link to="/prominent">Prominent</Link>
           <Link to="/committees">Committees</Link>
         </nav>
+        <Status />
     </footer>
 )
 
--- src/App/MainScreen/MainScreen.js
+++ src/App/MainScreen/MainScreen.js
@@ -2,13 +2,16 @@ import React from 'react'
 import faker from 'faker'
 
 faker.seed(123)
-const members = [...Array(1000)].map((idx) =>
-  faker.fake("{{name.lastName}}, {{name.firstName}}")
-)
+const members = [...Array(1000).keys()].map((idx) => {
+  return {
+    fullName: faker.fake("{{name.lastName}}, {{name.firstName}}"),
+    id: idx,
+  }
+})
 
 const MainScreen = () => (
   <ul>
-  {members.map((member, idx) => (<li key={idx}>{member}</li>))}
+  {members.map((member, idx) => (<li key={member.id}>{member.fullName}</li>))}
   </ul>
 )
 
