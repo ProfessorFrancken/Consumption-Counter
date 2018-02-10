2256f58fc2932bdb9237b5415565c99ec03e3866
Mark Redeman <markredeman@gmail.com>
2018-02-10 22:45:24 +0100

    Sort members by surname

--- src/App/MainScreen/MainScreen.js
+++ src/App/MainScreen/MainScreen.js
@@ -2,19 +2,23 @@ import React from 'react'
 import faker from 'faker'
 import './MainScreen.css'
 import LinkButton from './../LinkButton'
+import { sortBy } from 'underscore'
 
 faker.seed(123)
-const members = [...Array(6 * 7).keys()].map((idx) => {
-  const surname = faker.name.lastName();
-  const firstName = faker.name.firstName();
-  return {
-    surname,
-    fullName: `${firstName} ${surname}`,
-    id: idx,
-    birthday: undefined,
-    cosmetics: undefined
-  }
-})
+const members = sortBy(
+  [...Array(6 * 7).keys()].map((idx) => {
+    const surname = faker.name.lastName();
+    const firstName = faker.name.firstName();
+    return {
+      surname,
+      fullName: `${firstName} ${surname}`,
+      id: idx,
+      birthday: undefined,
+      cosmetics: undefined
+    }
+  }),
+  (member) => member.surname
+);
 
 const MainScreen = () => (
   <div className="MainScreen h-100 py-3">
