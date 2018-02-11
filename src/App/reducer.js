7ce8f05d9d58f2318c5eb66029f4e257d554dce1
Mark Redeman <markredeman@gmail.com>
2018-02-11 21:45:15 +0100

    Move default stata data into separate file

--- src/App/reducer.js
+++ src/App/reducer.js
@@ -1,25 +1,7 @@
 import producten from './../assets/products.json'
 import { sortBy, groupBy } from 'lodash'
 import faker from 'faker'
-
-const defaultProductsState = groupBy(
-  sortBy(
-    producten.map(product => {
-      return {
-        id: product.id,
-        name: product.naam,
-        price: product.prijs,
-        position: product.positie,
-        category: product.categorie,
-        image: product.afbeelding,
-        age_restriction: 18
-      }
-    }),
-    // Important products have a low number, so sort ascending
-    (product) => product.positie
-  ),
-  (product) => product.category
-);
+import { products as defaultProductsState, members as defaultMembersState } from './default_data.js'
 
 export function products(state = defaultProductsState, action) {
   switch (action.type) {
@@ -28,24 +10,7 @@ export function products(state = defaultProductsState, action) {
   }
 }
 
-faker.seed(123)
-const defaultMembers = sortBy(
-  [...Array(6 * 7).keys()].map((idx) => {
-    const surname = faker.name.lastName();
-    const firstName = faker.name.firstName();
-    return {
-      firstName,
-      surname,
-      fullName: `${firstName} ${surname} ${surname}`,
-      id: idx,
-      birthday: undefined,
-      cosmetics: undefined
-    }
-  }),
-  (member) => member.surname
-);
-console.log(defaultMembers)
-export function members(state = defaultMembers, action) {
+export function members(state = defaultMembersState, action) {
   return state;
 }
 
