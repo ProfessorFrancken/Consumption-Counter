151fbcffe79b870078c7292a15f12862882ed823
Mark Redeman <markredeman@gmail.com>
2018-02-11 17:34:54 +0100

    Simplify products grid css

--- src/Selection/Products.js
+++ src/Selection/Products.js
@@ -1,53 +1,20 @@
 import React from 'react'
-import faker from 'faker'
-import LinkButton from './../App/LinkButton'
-import { sortBy, groupBy } from 'lodash'
-import producten from './../assets/products.json'
-import '../App/MainScreen/MainScreen.css'
 import './Products.css'
 
-const products = groupBy(
-  sortBy(producten.map(product => {
-  return {
-    id: product.id,
-    name: product.naam,
-    price: product.prijs,
-    position: product.positie,
-    category: product.categorie,
-    image: product.afbeelding
-  }
-  }), (product) => product.positie)
-  , (product) => product.category);
+const ProductName = ({ product }) => (
+    <span className="btn-product-overlay">
+      {product.name}
+    </span>
+)
 
 const Product = ({ product, onClick }) => (
   <button
-    key={product.id}
     onClick={onClick}
-    className="SelectionItem btn btn-outline-light d-flex flex-column justify-content-center"
+    className="SelectionItem btn btn-outline-light d-flex flex-column justify-content-center btn-product"
     to="/products"
-    style={{
-      color: "#333",
-      backgroundColor: "white",
-      backgroundImage: `url(https:/old.professorfrancken.nl/database/streep/afbeeldingen/${product.image})`,
-      backgroundSize: "cover",
-      backgroundPosition: "50% 50%",
-      position: "relative"
-    }}
+    style={{ backgroundImage: `url(https:/old.professorfrancken.nl/database/streep/afbeeldingen/${product.image})` }}
   >
-    <span
-      style={{
-        backgroundColor: "rgba(0, 0, 0, 0.7)",
-        color: "rgba(255, 255, 255, 0.9)",
-        padding: ".5em",
-        position: "absolute",
-        fontWeight: "bold",
-        bottom: 0,
-        left: 0,
-        right: 0
-      }}
-    >
-      {product.name}
-    </span>
+    <ProductName product={product} />
   </button>
 )
 
@@ -57,17 +24,17 @@ const Products = ({ products = {} }) => {
 
   return (
     <div className="h-100 d-flex flex-row">
-      <nav className="SelectionGrid SelectionGrid-products">
+      <nav className="SelectionGrid products-grid">
         {products["Bier"].map((product, idx) => (
           <Product product={product} />
         ))}
       </nav>
-      <nav className="SelectionGrid SelectionGrid-products" style={{margin: "0 1%"}}>
+      <nav className="SelectionGrid products-grid">
         {products["Fris"].map((product, idx) => (
           <Product product={product} />
         ))}
       </nav>
-      <nav className="SelectionGrid SelectionGrid-products">
+      <nav className="SelectionGrid products-grid">
         {products["Eten"].map((product, idx) => (
           <Product product={product} />
         ))}
