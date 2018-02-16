833b8996b076e7d26a21d53623ec0023443507b5
Mark Redeman <markredeman@gmail.com>
2018-02-16 23:00:49 +0100

    Add Status component

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -3,13 +3,7 @@ import { Route } from 'react-router-dom'
 import './Footer.css';
 import LinkButton from './LinkButton'
 import GoBack from './GoBack'
-
-// Add on click which shows settings menu
-const Status = () => (
-  <div className="Footer-status">
-    <strong>Status:</strong> <span className="text-success">connected</span>
-  </div>
-)
+import Status from './Status'
 
 const Footer = () => (
     <footer className="Footer">
