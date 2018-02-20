05818d9028621b044fee3d52363f959ee747683d
Mark Redeman <markredeman@gmail.com>
2018-02-20 17:33:19 +0100

    Sort boards by their board year modulo the amount of boards we're showing

--- src/App/Prominent/index.js
+++ src/App/Prominent/index.js
@@ -13,21 +13,24 @@ const loadBoards = (boardMembers, members) => {
     };
   });
 };
-const SHOW_N_BOARDS = 6;
+
+const SHOW_N_BOARDS = 5;
 const prominent = state => {
-  const boards = take(
-    sortBy(
-      groupBy(
-        loadBoards(state.boardMembers, state.members),
-        boardMember => boardMember.year
+  const boards = sortBy(
+    take(
+      sortBy(
+        groupBy(
+          loadBoards(state.boardMembers, state.members),
+          boardMember => boardMember.year
+        ),
+        board => -first(board).year
       ),
-      board => -first(board).year
+      SHOW_N_BOARDS
     ),
-    SHOW_N_BOARDS
+    board => -((first(board).year + 1) % SHOW_N_BOARDS)
   );
 
   const boardMembersId = boards.reduce((members, board) => {
-    console.log(members, board);
     return [...members, ...board.map(member => member.member.id)];
   }, []);
 
