602fa8d213245b0edc72c6dc1f17df80af5257ee
Mark Redeman <markredeman@gmail.com>
2018-02-24 18:20:24 +0100

    Use english formated prices
    This is mostly to fix the tests on travis. Though additionally we don't really
    care about what format to use..

--- src/App/PriceList/Price.js
+++ src/App/PriceList/Price.js
@@ -1,18 +1,17 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Price = ({ price }) => (
   <span>
-  {
-    new Intl.NumberFormat(
-      'nl-NL', { style: 'currency', currency: 'EUR' }
-    ).format(price / 100)
-  }
+    {new Intl.NumberFormat('en-EN', {
+      style: 'currency',
+      currency: 'EUR'
+    }).format(price / 100)}
   </span>
-)
+);
 
 Price.propTypes = {
   price: PropTypes.number.isRequired
-}
+};
 
-export default Price
+export default Price;
--- src/App/Transactions/Price.test.js
+++ src/App/Transactions/Price.test.js
@@ -1,23 +1,17 @@
-import React from 'react'
-import Price from './Price'
-import { mount } from 'enzyme'
+import React from 'react';
+import Price from './Price';
+import { mount } from 'enzyme';
 
 describe('displaying the price of products', () => {
   it('shows a formated price for 1 product', () => {
-    const price = mount(
-      <Price products={[{ price: 11 }]} />
-    );
-    expect(price.text()).toEqual("€ 0,11")
-  })
+    const price = mount(<Price products={[{ price: 11 }]} />);
+    expect(price.text()).toEqual('€0.11');
+  });
 
   it('shows a formated price for multiple product', () => {
     const price = mount(
-      <Price products={[
-        { price: 11 },
-        { price: 11 },
-        { price: 11 }
-      ]} />
+      <Price products={[{ price: 11 }, { price: 11 }, { price: 11 }]} />
     );
-    expect(price.text()).toEqual("€ 0,33")
-  })
-})
+    expect(price.text()).toEqual('€0.33');
+  });
+});
