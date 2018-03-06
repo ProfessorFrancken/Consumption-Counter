b11cdce480e3df77753c14d98aa321af2764f630
Unknown <g.wielink.1@student.rug.nl>
2018-03-06 21:53:08 +0100

    Active MenuButtons

--- src/App/Sidebar.js
+++ src/App/Sidebar.js
@@ -1,17 +1,24 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import React from 'react';
 import Icon from './Icon';
 
 const Sidebar = ({ menuItems }) => (
   <div className="sidebar">
-    {menuItems.map((menuItem, i) => <MenuItem key={menuItem.icon} menuItem={menuItem} />)}
+    {menuItems.map((menuItem, i) => (
+      <MenuItem key={menuItem.icon} menuItem={menuItem} />
+    ))}
   </div>
 );
 
 const MenuItem = ({ menuItem }) => (
-  <Link to={menuItem.url} className="button menuButton" activeClassName="active">
+  <NavLink
+    exact
+    to={menuItem.url}
+    className="button menuButton"
+    activeClassName="active"
+  >
     <Icon name={menuItem.icon} />
-  </Link>
+  </NavLink>
 );
 
 export default Sidebar;
