75adb4e6c9c4af5ccfb36843a3e687700a97cc1d
Mark Redeman <markredeman@gmail.com>
2018-03-07 11:05:54 +0100

    When buying multiple products, show a counter for each product bought

--- src/App/Transactions/Transactions.js
+++ src/App/Transactions/Transactions.js
@@ -1,32 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Price from './Price'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Price from './Price';
+import { groupBy, map } from 'lodash';
+
+// Show all products that were bought and the amount of times they were bought
+const listOfProducts = products =>
+  map(
+    groupBy(products, product => product.id),
+    product =>
+      product.length === 1
+        ? `${product[0].name}`
+        : `${product[0].name} (${product.length}x)`
+  ).join(', ');
 
 const Transaction = ({ member, order }) => (
   <span>
-    {member.firstName} {member.surname}
-    {' '}
-    bought
-    {' '}
-    {order.products.map((product) => product.name ).join(',')}
-    {' '}
-    for
-    {' '}
-    <Price products={order.products} price={
-      order.products.map((product) => product.price ).reduce((sum, price) => sum + price, 0)
-    }  />
+    {member.firstName} {member.surname} bought {listOfProducts(order.products)}{' '}
+    for{' '}
+    <Price
+      products={order.products}
+      price={order.products
+        .map(product => product.price)
+        .reduce((sum, price) => sum + price, 0)}
+    />
   </span>
-)
+);
 
 const Transactions = ({ transactions }) => (
   <ol>
-    {transactions.map((transaction, idx) =>
+    {transactions.map((transaction, idx) => (
       <li key={idx}>
-        <Transaction {...transaction}/>
+        <Transaction {...transaction} />
       </li>
-    )}
+    ))}
   </ol>
-)
+);
 
 const TransactionPropType = PropTypes.shape({
   member: PropTypes.shape({}).isRequired,
@@ -34,16 +42,14 @@ const TransactionPropType = PropTypes.shape({
     products: PropTypes.arrayOf(
       PropTypes.shape({
         name: PropTypes.string.isRequired,
-        price: PropTypes.number.isRequired,
+        price: PropTypes.number.isRequired
       })
     )
-  }).isRequired,
+  }).isRequired
 });
 
 Transactions.propTypes = {
-  transactions: PropTypes.arrayOf(
-    TransactionPropType.isRequired
-  ).isRequired,
-}
+  transactions: PropTypes.arrayOf(TransactionPropType.isRequired).isRequired
+};
 
-export default Transactions
+export default Transactions;
