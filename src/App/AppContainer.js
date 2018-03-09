cfd4febbd976d44246b7e720744599dc346b173d
Mark Redeman <markredeman@gmail.com>
2018-03-09 16:06:47 +0100

    Update code styling and add selector to AppContainer

--- src/App/AppContainer.js
+++ src/App/AppContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import { fetchInitialData } from './../actions';
 import { withRouter } from 'react-router-dom';
 import { push } from 'react-router-redux';
@@ -15,29 +16,28 @@ class AppContainer extends Component {
   }
 }
 
-const backgroundFromOrder = order => {
+const orderSelector = state => state.queuedOrder;
+const backgroundSelector = createSelector(orderSelector, order => {
   if (order === null) {
     return null;
   }
+
   const product = order.order.products.find(
     product => product.splash_image !== null
   );
 
   return product === undefined ? null : product.splash_image;
-};
+});
 
-const mapStateToProps = state => {
-  return {
-    title: state.title,
-    background: backgroundFromOrder(state.queuedOrder)
-  };
-};
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchData: () => dispatch(fetchInitialData()),
-    goToCompucieScreen: () => dispatch(push('/compucie'))
-  };
-};
+const mapStateToProps = state => ({
+  title: state.title,
+  background: backgroundSelector(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchData: () => dispatch(fetchInitialData()),
+  goToCompucieScreen: () => dispatch(push('/compucie'))
+});
 
 export default withRouter(
   connect(mapStateToProps, mapDispatchToProps)(AppContainer)
--- src/App/GoBack.js
+++ src/App/GoBack.js
@@ -1,19 +1,21 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { goBack } from './../actions'
+import React from 'react';
+import { connect } from 'react-redux';
+import { goBack } from './../actions';
 
 const GoBack = ({ onClick }) => (
   <div className="Footer-go-back">
-    <button className="btn btn-outline-light btn-lg btn-block" to="/" onClick={onClick}>
+    <button
+      className="btn btn-outline-light btn-lg btn-block"
+      to="/"
+      onClick={onClick}
+    >
       Go back
     </button>
   </div>
-)
+);
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onClick: () => dispatch(goBack())
-  }
-};
+const mapDispatchToProps = dispatch => ({
+  onClick: () => dispatch(goBack())
+});
 
-export default connect(undefined, mapDispatchToProps)(GoBack)
+export default connect(undefined, mapDispatchToProps)(GoBack);
