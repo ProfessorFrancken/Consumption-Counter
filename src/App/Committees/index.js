1488b1d9eeda108bd8769a10cd387caed9f55512
Mark Redeman <markredeman@gmail.com>
2018-03-09 09:41:50 +0100

    Use selectors when getting committees
    Additionally show unique committee members, so that if a member joins a
    committee in two different years, they are only showed once.

--- src/App/Committees/index.js
+++ src/App/Committees/index.js
@@ -1,47 +1,64 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import { selectCommittee } from '../../actions';
 import Committees from './Committees';
 import { uniqBy } from 'lodash';
 
-const committees = (committeeMembers, members) => {
-  return uniqBy(
-    committeeMembers.reduce((committees, member) => {
-      return [...committees, member.committee];
-    }, []),
-    committee => committee.id
-  ).map(committee => {
-    return {
-      ...committee,
-      members: committeeMembers
-        .filter(member => member.committee.id === committee.id)
-        .map(member => {
-          return members.find(m => m.id === member.member_id);
-        })
-        .filter(member => member !== undefined)
-    };
-  });
+const activeMembersSelector = state => {
+  const year = (new Date()).getUTCFullYear();
+
+  return state.committeeMembers.filter(member =>
+    [year, year - 1].includes(member.year)
+  )
 };
 
-const mapStateToProps = state => {
-  // Only show committees and ther members of the current academic year
-  const today = new Date();
+const membersSelector = state => state.members;
 
-  return {
-    committees: committees(
-      state.committeeMembers.filter(member =>
-        [today.getUTCFullYear(), today.getUTCFullYear() - 1].includes(
-          member.year
-        )
+// From the list of all committee members, select all committees
+const committeesSelector = createSelector(
+  activeMembersSelector,
+  activeMembers =>
+    uniqBy(
+      activeMembers.reduce(
+        (committees, member) => [...committees, member.committee],
+        []
       ),
-      state.members
+      committee => committee.id
     )
-  };
-};
+);
 
-const mapDispatchToProps = dispatch => {
-  return {
-    selectCommittee: committee => dispatch(selectCommittee(committee))
-  };
-};
+// Get member info of all active members and filter out all members who don't streep
+const committeeeMembersWithMemberSelector = createSelector(
+  activeMembersSelector,
+  membersSelector,
+  (activeMembers, members) =>
+    activeMembers
+      .map(activeMember => ({
+        committee_id: activeMember.committee.id,
+        ...members.find(member => member.id === activeMember.member_id)
+      }))
+      .filter(member => member.id !== undefined)
+);
+
+const committeesWithMembersSelector = createSelector(
+  committeesSelector,
+  committeeeMembersWithMemberSelector,
+  (committees, members) =>
+    committees.map(committee => ({
+      ...committee,
+      members: uniqBy(
+        members.filter(member => member.committee_id === committee.id),
+        member => member.id
+      )
+    }))
+);
+
+const mapStateToProps = state => ({
+  committees: committeesWithMembersSelector(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  selectCommittee: committee => dispatch(selectCommittee(committee))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Committees);
--- src/App/Products/AvailableProducts.js
+++ src/App/Products/AvailableProducts.js
@@ -1,33 +1,44 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import { mapValues } from 'lodash';
 import { addProductToOrder } from '../../actions';
 import Products from './Products';
 
-const getAvailableProducts = (products, order) =>
-  mapValues(products, products =>
-    products
-      .filter(product => product.age_restriction <= order.member.age)
-      .map(product => {
-        return {
+const categorySelector = state => state.products;
+const orderSelector = state => state.order;
+
+// Select products that the selected member is allowed to buy
+const allowedProductsSelector = createSelector(
+  categorySelector,
+  orderSelector,
+  (categories, order) =>
+    mapValues(categories, products =>
+      products.filter(product => product.age_restriction <= order.member.age)
+    )
+);
+
+const productsWithOrderCountSelector = createSelector(
+  allowedProductsSelector,
+  orderSelector,
+  (categories, order) =>
+    mapValues(
+      categories,
+      // For each product in the category, count the mount of times it was ordered
+      products =>
+        products.map(product => ({
           ...product,
           ordered: order.products.filter(p => p.id === product.id).length
-        };
-      })
-  );
+        }))
+    )
+);
 
-const mapStateToProps = state => {
-  return {
-    products: getAvailableProducts(state.products, state.order)
-  };
-};
+const mapStateToProps = state => ({
+  products: productsWithOrderCountSelector(state)
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addProductToOrder: product => {
-      dispatch(addProductToOrder(product));
-    }
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  addProductToOrder: product => dispatch(addProductToOrder(product))
+});
 
 const AvailableProducts = connect(mapStateToProps, mapDispatchToProps)(
   Products
