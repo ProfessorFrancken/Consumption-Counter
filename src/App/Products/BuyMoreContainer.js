46b2684432ce829fa4af270a10fdfed5ce862ee2
Mark Redeman <markredeman@gmail.com>
2018-03-09 09:49:17 +0100

    Simplify BuyMoreContainer

--- src/App/Products/BuyMoreContainer.js
+++ src/App/Products/BuyMoreContainer.js
@@ -2,18 +2,14 @@ import BuyMore from './BuyMore';
 import { connect } from 'react-redux';
 import { buyMore, buyAll } from './../../actions';
 
-const mapStateToProps = ({ order }) => {
-  return {
-    buyMore: order.buyMore,
-    selectedMultipleProducts: order.buyMore && order.products.length > 0
-  };
-};
+const mapStateToProps = ({ order }) => ({
+  buyMore: order.buyMore,
+  selectedMultipleProducts: order.buyMore && order.products.length > 0
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    toggle: () => dispatch(buyMore()),
-    buyAll: () => dispatch(buyAll())
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  toggle: () => dispatch(buyMore()),
+  buyAll: () => dispatch(buyAll())
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(BuyMore);
