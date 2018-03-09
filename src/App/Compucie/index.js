a62e243da9fa14200e7fd583d13c7ca7133e5b7c
Mark Redeman <markredeman@gmail.com>
2018-03-09 17:05:39 +0100

    Use a selector for the compucie screen

--- src/App/Compucie/index.js
+++ src/App/Compucie/index.js
@@ -1,41 +1,49 @@
 import { connect } from 'react-redux';
 import { selectMember } from '../../actions';
 import { uniqBy, groupBy } from 'lodash';
+import { createSelector } from 'reselect';
 import Compucie from './Compucie';
 
+const membersSelector = state => state.members;
+const committeeMembersSelector = state => state.committeeMembers;
+
+export const compucieSelector = createSelector(
+  membersSelector,
+  committeeMembersSelector,
+  (members, committeeMembers) => {
+    const committees = groupBy(
+      uniqBy(
+        committeeMembers
+          .filter(member => {
+            return ['Compucie', 's[ck]rip(t|t?c)ie'].includes(
+              member.committee.name
+            );
+          })
+          .map(member => {
+            const m = members.find(m => m.id === member.member_id);
+            return m !== undefined
+              ? { ...m, name: member.committee.name }
+              : undefined;
+          })
+          .filter(m => m !== undefined),
+        member => member.id
+      ),
+      member => member.name
+    );
+
+    return {
+      compucie: committees['Compucie'] || [],
+      scriptcie: committees['s[ck]rip(t|t?c)ie'] || []
+    };
+  }
+);
+
 const mapStateToProps = state => {
-  const committees = groupBy(
-    uniqBy(
-      state.committeeMembers
-        .filter(member => {
-          return ['Compucie', 's[ck]rip(t|t?c)ie'].includes(
-            member.committee.name
-          );
-        })
-        .map(member => {
-          const m = state.members.find(m => m.id === member.member_id);
-          return m !== undefined
-            ? { ...m, name: member.committee.name }
-            : undefined;
-        })
-        .filter(m => m !== undefined),
-      member => member.id
-    ),
-    member => member.name
-  );
-
-  return {
-    compucie: committees['Compucie'] || [],
-    scriptcie: committees['s[ck]rip(t|t?c)ie'] || []
-  };
+  return compucieSelector(state);
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    selectMember: member => {
-      dispatch(selectMember(member));
-    }
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  selectMember: member => dispatch(selectMember(member))
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Compucie);
