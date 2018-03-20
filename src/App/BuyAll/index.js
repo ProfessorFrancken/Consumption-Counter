0455604d718a304b0c6a70deeac101f725ac27ca
Mark Redeman <markredeman@gmail.com>
2018-03-20 15:54:10 +0100

    Show the price when buying multiple products

--- src/App/BuyAll/index.js
+++ src/App/BuyAll/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { buyAll } from '../../actions';
+import Price from './../Transactions/Price';
 import Icon from './../Icon';
 
-const BuyAll = ({ buyAll, buyMore }) =>
+const BuyAll = ({ buyAll, buyMore, products }) =>
   !buyMore ? null : (
     <button className="button buyAllButton" onClick={buyAll}>
       <Icon name="check-circle" />
-      <span style={{ marginLeft: '.5em' }}>Buy it all!</span>
+      <span style={{ marginLeft: '.5em' }}>
+        Buy it all! (<Price products={products} />)
+      </span>
     </button>
   );
 
 const mapStateToProps = ({ order }) => ({
-  buyMore: order.buyMore
+  buyMore: order.buyMore,
+  products: order.products
 });
 
 const mapDispatchToProps = dispatch => ({
