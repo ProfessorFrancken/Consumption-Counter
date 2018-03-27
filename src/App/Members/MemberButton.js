78a3ee8afebf9ebca6b5dd28b897929930ca8911
Mark Redeman <markredeman@gmail.com>
2018-03-27 12:05:41 +0200

    Make Nina's button small

--- src/App/Members/MemberButton.js
+++ src/App/Members/MemberButton.js
@@ -1,20 +1,28 @@
 import React from 'react';
 
+const isSet = property => ![undefined, null, 0].includes(property);
+const smallButton = (button = {}) =>
+  isSet(button.width) || isSet(button.height)
+    ? { transform: 'scale(0.5)' }
+    : {};
+const buttonStyle = member => ({
+  backgroundColor: member.cosmetics.color,
+  backgroundImage: member.cosmetics.image
+    ? `url(${member.cosmetics.image})`
+    : undefined,
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: '50% 50%',
+  flexGrow: 1,
+  ...smallButton(member.cosmetics.button)
+});
+
 const Member = ({ member, onClick, style = {} }) => (
   <button
     key={member.id}
     className="tile button"
     onClick={() => onClick(member)}
-    style={{
-      backgroundColor: member.cosmetics.color,
-      backgroundImage: member.cosmetics.image
-        ? `url(${member.cosmetics.image})`
-        : undefined,
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: '50% 50%',
-      flexGrow: 1
-    }}
+    style={buttonStyle(member)}
   >
     {member.cosmetics.nickname
       ? member.cosmetics.nickname
