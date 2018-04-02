7c3176d9d1f405110fbc8cb9a125a61c7cc56966
Mark Redeman <markredeman@gmail.com>
2018-04-02 12:29:29 +0200

    Apply prettier styles to the queued orders view

--- src/App/Settings/index.js
+++ src/App/Settings/index.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import Authenticate from './Authentication';
 import Price from './../Transactions/Price';
 import { groupBy, map } from 'lodash';
-import moment from 'moment';
 import { cancelOrder, buyOrder } from './../../actions';
+import Moment from 'react-moment';
 
 // Show all products that were bought and the amount of times they were bought
 const listOfProducts = products =>
@@ -16,44 +16,55 @@ const listOfProducts = products =>
         : `${product[0].name} (${product.length}x)`
   ).join(', ');
 
-const status = order => order.state;
+const status = order =>
+  order.state + (order.fails > 0 ? ` (failed ${order.fails}x)` : '');
 
 const Settings = ({ orders, cancel, buy }) => (
   <div>
     <Authenticate />
-    <div>
-      <h2>Queud Orders</h2>
-      <table className="table">
-        <thead>
+    <div className="mb-5 bg-light">
+      <h2 className="h4 font-weight-normal p-3">Queud Orders</h2>
+      <table className="table table-hover">
+        <thead className="thead-light">
           <tr>
             <th scope="col">Ordered at</th>
             <th scope="col">Member</th>
             <th scope="col">Products</th>
-            <th scope="col">Price</th>
-            <th scope="col">Status</th>
-            <th scope="col">Actions</th>
+            <th scope="col" className="text-right">
+              Price
+            </th>
+            <th scope="col" className="text-right">
+              Status
+            </th>
+            <th scope="col" className="text-right">
+              Actions
+            </th>
           </tr>
         </thead>
         <tbody>
           {orders.map((order, idx) => (
             <tr key={idx}>
-              <th scope="row">{moment(order.order.ordered_at).calendar()}</th>
+              <th scope="row">
+                <Moment fromNow interval={1000} unix>
+                  {order.order.ordered_at / 1000}
+                </Moment>
+              </th>
               <td>{order.order.member.fullname}</td>
               <td>{listOfProducts(order.order.products)}</td>
-              <td>
+              <td className="text-right">
                 <Price products={order.order.products} />
               </td>
-              <td>{status(order)}</td>
-              <td>
+              <td className="text-right">{status(order)}</td>
+              <td className="text-right">
                 <button
-                  className="btn btn-outline-success mr-2"
+                  className="btn btn-link text-success mr-2"
                   onClick={() => buy(order.order)}
                 >
                   Retry now
                 </button>
 
                 <button
-                  className="btn btn-outline-danger"
+                  className="btn btn-link text-danger"
                   onClick={() => cancel(order.order)}
                 >
                   Cancel
