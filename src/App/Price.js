5f5ddc0307491739aecb37d6a7036b2476de0eae
Mark Redeman <markredeman@gmail.com>
2018-07-17 01:31:40 +0200

    Add Price.js
    This was moved from the deleted Transactions folder.

---
+++ src/App/Price.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const totalPrice = products =>
+  products.map(product => product.price).reduce((sum, price) => sum + price, 0);
+
+const Price = ({ products }) => (
+  <span>
+    {new Intl.NumberFormat('en-EN', {
+      style: 'currency',
+      currency: 'EUR'
+    }).format(totalPrice(products) / 100)}
+  </span>
+);
+
+export default Price;
