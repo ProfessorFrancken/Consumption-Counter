fd2839ff09989cbd96d26160c292656ff3d54b77
Mark Redeman <markredeman@gmail.com>
2018-08-31 12:23:24 +0200

    Show compucie options at the top of the screen

--- src/App/Compucie/Compucie.js
+++ src/App/Compucie/Compucie.js
@@ -13,12 +13,6 @@ const Compucie = ({
   decreaseTempleCount
 }) => (
   <div className="d-flex flex-column justify-content-stretch h-100">
-    <nav className="compucie tilesGrid" style={{ flexShrink: 1 }}>
-      {[...compucie, ...scriptcie].map(member => (
-        <Member member={member} key={member.id} onClick={selectMember} />
-      ))}
-    </nav>
-
     <nav className="tilesGrid" style={{ flexShrink: 4, marginTop: '1em' }}>
       <Member
         member={{
@@ -32,39 +26,16 @@ const Compucie = ({
         onClick={() => window.location.reload()}
       />
 
-      <Member
-        member={{
-          id: 1098,
-          fullname: 'Guest',
-          age: 101,
-          prominent: 0,
-          latest_purchase_at: new Date(),
-          cosmetics: {}
-        }}
-        onClick={selectMember}
-      />
-
-      <Member
-        member={{
-          id: 1098,
-          fullname: 'Overdue',
-          age: 101,
-          prominent: 0,
-          latest_purchase_at: new Date(),
-          cosmetics: {}
-        }}
-        onClick={selectMember}
-      />
-
       <TempleCountButton decreaseTempleCount={decreaseTempleCount} />
-      <div>
-        {/* This div is deliberately left empty so that the settings
-               button will be placed at the right most part of the grid
-             */}
-      </div>
       <NavLink exact to="/settings" className="tile button">
         <FontAwesomeIcon icon={'cogs'} size="lg" />
       </NavLink>
+    </nav>
+
+    <nav className="compucie tilesGrid" style={{ flexShrink: 1 }}>
+      {[...compucie, ...scriptcie].map(member => (
+        <Member member={member} key={member.id} onClick={selectMember} />
+      ))}
     </nav>
   </div>
 );
