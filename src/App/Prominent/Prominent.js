9282984e91e766c9651a49362e1e53c6fb20083f
Mark Redeman <markredeman@gmail.com>
2018-08-31 12:05:25 +0200

    Optimize prominent page for Pienter

--- src/App/Prominent/Prominent.js
+++ src/App/Prominent/Prominent.js
@@ -4,27 +4,18 @@ import Member from './../Members/MemberButton';
 import { sortBy } from 'lodash';
 
 const Prominent = ({ prominent, boards, selectMember }) => (
-  <div className="prominentGrid">
-    <div className="prominentRow">
-      {prominent.map(member => (
-        <Member member={member} key={member.id} onClick={selectMember} />
-      ))}
-    </div>
-    <div className="boardsRow">
-      {boards.map((members, idx) => (
-        <div className="boardColumn" key={idx}>
-          {sortBy(members, member => member.function)
-            .reverse()
-            .map(member => (
-              <Member
-                member={member.member}
-                key={member.member.id}
-                onClick={selectMember}
-              />
-            ))}
-        </div>
-      ))}
-    </div>
+  <div className="tilesGrid">
+    {boards
+      .reduce((members, boardMembers) => [...members, ...boardMembers], [])
+      .map((member, idx) => {
+        return (
+          <Member
+            member={member.member}
+            key={member.member.id}
+            onClick={selectMember}
+          />
+        );
+      })}
   </div>
 );
 
