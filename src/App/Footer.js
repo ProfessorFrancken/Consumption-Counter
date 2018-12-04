7fe1c9ec4be1cfe0eb6cae132bb36ecbbd477d4b
Mark Redeman <markredeman@gmail.com>
2018-12-04 20:00:19 +0100

    Add Demcon logo to the footer

--- src/App/Footer.js
+++ src/App/Footer.js
@@ -2,12 +2,19 @@ import React from 'react';
 import BuyAll from './BuyAll';
 import GoBack from './GoBack';
 import CancelOrder from './CancelOrder';
+import Demcon from './../assets/DEMCON_logo.png';
 
 const Footer = () => (
-  <div className="footer">
-    <CancelOrder />
-    <BuyAll />
-    <GoBack />
+  <div
+    className="footer justify-content-between"
+    style={{ paddingLeft: '110px' }}
+  >
+    <img src={Demcon} className="h-100 py-4" />
+    <div className="d-flex justify-content-end">
+      <CancelOrder />
+      <BuyAll />
+      <GoBack />
+    </div>
   </div>
 );
 
