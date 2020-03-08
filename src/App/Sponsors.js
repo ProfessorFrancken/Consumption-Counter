ac57c9ec4900e94e5fb0dca09ec752d38b131e34
Mark Redeman <markredeman@gmail.com>
2020-03-08 23:52:59 +0100

    Add company-logos class
    Used for testing

--- src/App/Sponsors.js
+++ src/App/Sponsors.js
@@ -7,7 +7,7 @@ import Thales from './../assets/sponsors/Thales-gray.png';
 import ASML from './../assets/sponsors/ASML.png';
 
 const Sponsors = () => (
-  <ul className="list-unstyled my-0 flex-grow-0 d-flex">
+  <ul className="company-logos list-unstyled my-0 flex-grow-0 d-flex">
     <li className="d-flex align-items-center mx-3">
       <img
         src={Demcon}
