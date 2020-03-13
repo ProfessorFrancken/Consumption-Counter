bd69cae5ae78afcdec63559988e7fe35ed588d08
Mark Redeman <markredeman@gmail.com>
2020-03-13 14:46:42 +0100

    Remove GerrieApp

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import GerrieApp from './Gerjan/App';
 import { AppContent } from './AppContent';
 import 'bootstrap/dist/css/bootstrap.css';
 import './FontAwesome';
+import Content from './Content';
+import Footer from './Footer';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import logo from './logo.png';
+
+const backgroundFromProduct = (background = null) => {
+  return background === null
+    ? {}
+    : {
+        backgroundImage: `url("${background}")`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: '50%'
+      };
+};
 
 const App = ({
   menuItems,
@@ -13,17 +28,29 @@ const App = ({
   goHome,
   failedOrders
 }) => (
-  <GerrieApp
-    menuItems={menuItems}
-    background={background}
-    title={title}
-    goToCompucieScreen={goToCompucieScreen}
-    goToProminent={goToProminent}
-    goHome={goHome}
-    failedOrders={failedOrders}
-  >
-    <AppContent />
-  </GerrieApp>
+  <div className="wrapper" style={backgroundFromProduct(background)}>
+    <Header
+      title={title}
+      onClick={goToCompucieScreen}
+      failedOrders={failedOrders}
+    />
+    <Sidebar menuItems={menuItems} />
+    <Content>
+      <AppContent />
+    </Content>
+    <Footer />
+    <div className="arrow">
+      <div className="arrowContent">
+        <img
+          src={logo}
+          className="franckenLogo img-fluid"
+          alt="Logo of T.F.V. 'Professor Francken'"
+          onClick={goHome}
+          onDoubleClick={goToProminent}
+        />
+      </div>
+    </div>
+  </div>
 );
 
 export default App;
