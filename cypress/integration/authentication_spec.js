5f2c69141c94691bd718b12b4be9f3ec4c250d42
Mark Redeman <markredeman@gmail.com>
2020-03-13 14:24:15 +0100

    Separate authentication spec

---
+++ cypress/integration/authentication_spec.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-undef */
+import { makeServer } from '../../src/server/';
+import { TIME_TO_CANCEL } from './../../src/actions';
+import { SCREEN_SAVER_TIMEOUT } from './../../src/App/ScreenSaver';
+import { association } from 'miragejs';
+import moment from 'moment';
+
+const member = {
+  firstName: 'Joris',
+  lastName: 'Aaaaadmiraal',
+  fullName: 'Joris Aaaaadmiraal'
+};
+
+let server;
+beforeEach(() => {
+  server = makeServer({ environment: 'test' });
+  server.create('member', {
+    latest_purchase_at: '2020-03-08 22:05:49',
+    geboortedatum: '1993-04-26',
+    achternaam: member.lastName,
+    voornaam: member.firstName,
+    tussenvoegsel: '',
+    bijnaam: ''
+  });
+});
+
+afterEach(() => {
+  server.shutdown();
+});
+
+describe('Authentication', () => {
+  it('Authenticates with the plus one API', () => {
+    expect(localStorage.getItem('plus_one_authorization')).to.be.null;
+    const password = 'bitterballen';
+
+    cy.visit('/settings');
+    cy.get('.form-control').type(password);
+    cy.get('.mb-5.p-3')
+      .get('.btn')
+      .click()
+      .should(() => {
+        const encoded_token = localStorage.getItem('plus_one_authorization');
+        expect(encoded_token).to.not.be.null;
+
+        const token = JSON.parse(encoded_token);
+        expect(token).to.not.be.null;
+      });
+
+    cy.get('.mb-5.p-3')
+      .get('.btn')
+      .contains('Refresh token');
+
+    cy.visit('/');
+    cy.get('.tilesGrid').should('contain', member.lastName);
+  });
+});
