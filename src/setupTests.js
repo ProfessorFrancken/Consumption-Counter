19c59ee72b0901b481bda015a10509edcc027584
Mark Redeman <markredeman@gmail.com>
2020-03-13 11:52:37 +0100

    Install mutationobserver-shim

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,10 +1,11 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'mutationobserver-shim';
 
 const localStorageMock = {
-    getItem: jest.fn(),
-    setItem: jest.fn(),
-    clear: jest.fn()
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
 };
 global.localStorage = localStorageMock;
 
