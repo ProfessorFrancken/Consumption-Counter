7acddc1a0e84830c5f1d9680b817df9faefbfc6c
Mark Redeman <markredeman@gmail.com>
2020-03-15 23:21:54 +0100

    Rename SurnameRangesSelection

---
+++ src/App/SurnameRanges/index.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import SurnameRanges from './SurnameRanges';
+import { selectRangeOfSurnames } from './../../actions';
+import { rangesSelector } from './../../selectors';
+
+const mapStateToProps = state => ({
+  ranges: rangesSelector(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  selectRange: range => dispatch(selectRangeOfSurnames(range))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SurnameRanges);
