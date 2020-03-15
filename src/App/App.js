eee8e3c179365405ccc5d8eccbc51bec18326572
Mark Redeman <markredeman@gmail.com>
2020-03-15 21:09:56 +0100

    Show LoadingScreen without Layout

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,20 @@ import { Layout } from './../Layout/Layout';
 import { AppContent } from './AppContent';
 import 'bootstrap/dist/css/bootstrap.css';
 import './FontAwesome';
+import { useLocation } from 'react-router-dom';
 
-const App = props => (
-  <Layout {...props}>
-    <AppContent />
-  </Layout>
-);
+const App = props => {
+  const { pathname } = useLocation();
+
+  if (pathname === '/loading') {
+    return <AppContent />;
+  }
+
+  return (
+    <Layout {...props}>
+      <AppContent />
+    </Layout>
+  );
+};
 
 export default App;
