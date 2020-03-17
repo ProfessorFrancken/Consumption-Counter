7346317f22bc4603a42f1a9a47b631ad7e084333
Mark Redeman <markredeman@gmail.com>
2020-03-17 22:21:04 +0100

    Extract statistics reducers

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,10 @@
 import { TYPES } from 'actions';
 import { sortBy, groupBy, chunk, first, last, take  } from 'lodash';
-import moment from 'moment';
 export { loading } from 'Loading/reducers';
 export { menuItems } from 'Layout/Sidebar/reducers';
 export { recentBuyers } from 'App/Recent/reducers';
 export { committeeMembers } from 'App/Committees/reducers';
+export { statistics, activities } from 'App/Statistics/reducers';
 
 const product_images = [];
 const member_images = [];
@@ -197,53 +197,6 @@ export function queuedOrders(state = [], action) {
           ? { ...order, fails: order.fails + 1, state: 'queued' }
           : order;
       });
-    default:
-      return state;
-  }
-}
-
-export function statistics(state = [], action) {
-  switch (action.type) {
-    case TYPES.FETCH_STATISTICS_SUCCESS:
-      return action.statistics;
-    case TYPES.BUY_ORDER_SUCCESS:
-      const ordered_at = new Date(action.order.ordered_at);
-      ordered_at.setSeconds(0);
-      ordered_at.setMinutes(0);
-      ordered_at.setHours(0);
-
-      return state.map(statistic => {
-        if (statistic.date === moment(ordered_at).format('YYYY-MM-DD')) {
-          const beers = action.order.products.filter(
-            product => product.category === 'Bier'
-          ).length;
-          const soda = action.order.products.filter(
-            product => product.category === 'Fris'
-          ).length;
-          const food = action.order.products.filter(
-            product => product.category === 'Eten'
-          ).length;
-
-          return {
-            date: statistic.date,
-            total: statistic.total + action.order.products.length,
-            beer: statistic.beer + beers,
-            soda: statistic.soda + soda,
-            food: statistic.food + food
-          };
-        }
-
-        return statistic;
-      });
-    default:
-      return state;
-  }
-}
-
-export function activities(state = [], action) {
-  switch (action.type) {
-    case TYPES.FETCH_ACTIVITIES_SUCCESS:
-      return action.activities;
     default:
       return state;
   }
