de059294b715aa3e98fb8e87d7f355794b9edabf
Mark Redeman <markredeman@gmail.com>
2021-01-24 00:33:01 +0100

    Convert Header.test.js to testing-library/react

--- src/Layout/Header.test.js
+++ src/Layout/Header.test.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import {MemoryRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import Header from "./Header";
-import {mount} from "enzyme";
 import configureMockStore from "redux-mock-store";
+import {render} from "@testing-library/react";
 
 it("renders a buy more button when visiting the products page", () => {
   const mockStore = configureMockStore([]);
@@ -12,11 +12,13 @@ it("renders a buy more button when visiting the products page", () => {
     order: {buyMore: false, member: {fullname: "Mark Redeman"}},
     queuedOrder: null,
   });
-  const header = mount(
+  const {getByText} = render(
     <Provider store={store}>
       <MemoryRouter initialEntries={["/products"]}>
         <Header />
       </MemoryRouter>
     </Provider>
   );
+
+  expect(getByText("Show prices")).toBeDefined();
 });
