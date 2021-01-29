c719c255d1016e9f3c7849f18fc44aadfae2d75d
Mark Redeman <markredeman@gmail.com>
2021-01-29 19:32:22 +0100

    [TEST] Simplify Header test

--- src/Layout/Header.test.tsx
+++ src/Layout/Header.test.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import {MemoryRouter} from "react-router-dom";
-import {Provider} from "react-redux";
 import Header from "./Header";
-import configureMockStore from "redux-mock-store";
-import {render} from "@testing-library/react";
+import {render} from "test-utils";
 
 it("renders a buy more button when visiting the products page", () => {
-  const mockStore = configureMockStore([]);
-  const store = mockStore({
+  const storeState = {
     order: {buyMore: false, member: {fullname: "Mark Redeman"}},
     queuedOrder: null,
-  });
-  const {getByText} = render(
-    <Provider store={store}>
-      <MemoryRouter initialEntries={["/products"]}>
-        <Header />
-      </MemoryRouter>
-    </Provider>
-  );
+  };
+
+  const routes = ["/products"];
+
+  const {getByText} = render(<Header />, {storeState, routes});
 
   expect(getByText("Show prices")).toBeDefined();
 });
