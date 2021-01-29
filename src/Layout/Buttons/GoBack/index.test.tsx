cd6fae0d9be32fec0311234d9dc1b1c12083e1d2
Mark Redeman <markredeman@gmail.com>
2021-01-29 23:34:03 +0100

    [TEST] Simplify GoBack test

--- src/Layout/Buttons/GoBack/index.test.tsx
+++ src/Layout/Buttons/GoBack/index.test.tsx
@@ -1,73 +1,47 @@
 import React from "react";
 import GoBack from ".";
-import configureMockStore from "redux-mock-store";
-import {MemoryRouter} from "react-router-dom";
-import {TYPES} from "actions";
-import {push, goBack} from "connected-react-router";
-import thunk from "redux-thunk";
 import {render, fireEvent} from "test-utils";
-import {Provider} from "react-redux";
 
 describe("<GoBack />", () => {
   it("renders", () => {
-    const mockStore = configureMockStore([thunk]);
+    const storeState = {queuedOrder: null};
+    const routes = ["/products"];
 
-    const store = mockStore({queuedOrder: null});
-    const {getByRole} = render(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={["/products"]}>
-          <GoBack />
-        </MemoryRouter>
-      </Provider>
-    );
+    const {getByRole} = render(<GoBack />, {storeState, routes});
 
     fireEvent.click(getByRole("button"));
-
-    expect(store.getActions()).toEqual([goBack(), {type: TYPES.GO_BACK}]);
+    expect(getByRole("button")).toHaveTextContent("Go back");
   });
 
   it("goes back to a previous customer", () => {
-    const mockStore = configureMockStore([thunk]);
-
-    const member = {
-      id: 1,
-      firstName: "John",
-      surname: "Snow",
-      age: 18,
-      prominent: null,
-      latest_purchase_at: new Date(),
-      cosmetics: {
-        color: null,
-        image: null,
-        nickname: null,
-        button: {
-          width: null,
-          height: null,
-        },
-      },
-    };
-    const store = mockStore({
+    const storeState = {
       queuedOrder: {
         ordered_at: 1,
         order: {
-          member: member,
+          member: {
+            id: 1,
+            firstName: "John",
+            surname: "Snow",
+            age: 18,
+            prominent: null,
+            latest_purchase_at: new Date(),
+            cosmetics: {
+              color: null,
+              image: null,
+              nickname: null,
+              button: {
+                width: null,
+                height: null,
+              },
+            },
+          },
           products: [],
         },
       },
-    });
-    const {getByRole} = render(
-      <Provider store={store}>
-        <MemoryRouter>
-          <GoBack />
-        </MemoryRouter>
-      </Provider>
-    );
+    };
+    const {getByRole} = render(<GoBack />, {storeState});
 
     fireEvent.click(getByRole("button"));
-
-    expect(store.getActions()).toEqual([
-      push("/products"),
-      {type: TYPES.SELECT_MEMBER, member},
-    ]);
+    expect(getByRole("button")).toHaveTextContent("John Snow");
   });
 });
