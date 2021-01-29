eceb3038fbf6a2e8ae1213bff66a335debb57e8f
Mark Redeman <markredeman@gmail.com>
2021-01-29 19:56:28 +0100

    [TEST] Simplify Products test

--- src/App/Products/index.test.tsx
+++ src/App/Products/index.test.tsx
@@ -1,30 +1,19 @@
 import React from "react";
 import AvailableProducts from "./index";
-import configureMockStore from "redux-mock-store";
 import {render} from "test-utils";
-import {Provider} from "react-redux";
-import thunk from "redux-thunk";
 
 it("renders, and it does not include products that a member is not allowed to buy (due to age distriction)", () => {
-  const api = {
-    post: jest.fn(),
-  };
-  const mockStore = configureMockStore([thunk.withExtraArgument(api)]);
-
-  const store = mockStore({
+  const storeState = {
     products: {
       Bier: [{name: "Hertog Jan", image: "", id: 1, age_restriction: 18}],
       Fris: [{name: "Ice Tea", image: "", id: 2, age_restriction: null}],
       Eten: [],
     },
     order: {member: {age: 17}, products: []},
+  };
+  const {queryByLabelText, getByLabelText, getAllByRole} = render(<AvailableProducts />, {
+    storeState,
   });
-  const {queryByLabelText, getByLabelText, getAllByRole} = render(
-    <Provider store={store}>
-      {/* @ts-expect-error ts-migrate(2322) FIXME: Type '{ store: MockStoreEnhanced<unknown, {}>; }' ... Remove this comment to see the full error message */}
-      <AvailableProducts store={store} />
-    </Provider>
-  );
 
   expect(queryByLabelText("beer category")).toBeNull();
   expect(queryByLabelText("soda category")).not.toBeNull();
@@ -34,12 +23,7 @@ it("renders, and it does not include products that a member is not allowed to bu
 });
 
 it("shows the amount of products that are currently being orderd", () => {
-  const api = {
-    post: jest.fn(),
-  };
-  const mockStore = configureMockStore([thunk.withExtraArgument(api)]);
-
-  const store = mockStore({
+  const storeState = {
     products: {
       Bier: [{name: "Hertog Jan", image: "", id: 1, age_restriction: 18}],
       Fris: [{name: "Ice Tea", image: "", id: 2, age_restriction: null}],
@@ -60,14 +44,8 @@ it("shows the amount of products that are currently being orderd", () => {
         },
       ],
     },
-  });
-
-  const {getAllByLabelText} = render(
-    <Provider store={store}>
-      {/* @ts-expect-error ts-migrate(2322) FIXME: Type '{ store: MockStoreEnhanced<unknown, {}>; }' ... Remove this comment to see the full error message */}
-      <AvailableProducts store={store} />
-    </Provider>
-  );
+  };
+  const {getAllByLabelText} = render(<AvailableProducts />, {storeState});
 
   const orderedProducts = getAllByLabelText("amount ordered");
   expect(orderedProducts).toHaveLength(1);
