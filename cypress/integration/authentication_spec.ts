a5f3cbdbbc453ebe79f916b432b1c1de9504f0ed
Mark Redeman <markredeman@gmail.com>
2021-02-01 19:15:44 +0100

    Update authentication spec to use testing-library

--- cypress/integration/authentication_spec.ts
+++ cypress/integration/authentication_spec.ts
@@ -1,26 +1,22 @@
 /* eslint-disable no-undef */
-import { makeServer } from '../../src/server/';
-import { TIME_TO_CANCEL } from './../../src/actions';
-import { SCREEN_SAVER_TIMEOUT } from './../../src/App/ScreenSaver';
-import { association } from 'miragejs';
-import moment from 'moment';
+import {makeServer} from "../../src/server/";
 
 const member = {
-  firstName: 'Joris',
-  lastName: 'Aaaaadmiraal',
-  fullName: 'Joris Aaaaadmiraal'
+  firstName: "John",
+  lastName: "Snow",
+  fullName: "John SNow",
 };
 
 let server;
 beforeEach(() => {
-  server = makeServer({ environment: 'test' });
-  server.create('member', {
-    latest_purchase_at: '2020-03-08 22:05:49',
-    geboortedatum: '1993-04-26',
+  server = makeServer({environment: "test"});
+  server.create("member", {
+    latest_purchase_at: "2020-03-08 22:05:49",
+    geboortedatum: "1993-04-26",
     achternaam: member.lastName,
     voornaam: member.firstName,
-    tussenvoegsel: '',
-    bijnaam: ''
+    tussenvoegsel: "",
+    bijnaam: "",
   });
 });
 
@@ -28,33 +24,32 @@ afterEach(() => {
   server.shutdown();
 });
 
-describe('Authentication', () => {
-  it('Authenticates with the plus one API', () => {
-    expect(localStorage.getItem('plus_one_authorization')).to.be.null;
-    const password = 'bitterballen';
+describe("Authentication", () => {
+  it("Authenticates with the plus one API", () => {
+    expect(localStorage.getItem("plus_one_authorization")).to.be.null;
+    const password = "bitterballen";
 
-    cy.visit('/settings');
-    cy.get('.form-control').type(password);
-    cy.get('.mb-5.p-3')
-      .get('.btn')
+    cy.visit("/");
+
+    cy.findByPlaceholderText("Passphrase").type(password);
+    cy.findByRole("button", {name: /Authenticat/i})
       .click()
       .should(() => {
-        const encoded_token = localStorage.getItem('plus_one_authorization');
+        const encoded_token = localStorage.getItem("plus_one_authorization");
         expect(encoded_token).to.not.be.null;
 
         const token = JSON.parse(encoded_token);
         expect(token).to.not.be.null;
       });
 
-    cy.location('pathname').should('eq', '/loading');
-    cy.get('.tile').should('contain', 'Open application');
-    cy.get('.tile').click();
-    cy.location('pathname').should('eq', '/');
-    cy.get('.tilesGrid').should('contain', member.lastName);
+    cy.location("pathname").should("eq", "/loading");
+    cy.findByText("Loading", {exact: false});
+    cy.findByRole("link", {name: /Open application/}).click();
 
-    cy.visit('/settings');
-    cy.get('.mb-5.p-3')
-      .get('.btn')
-      .contains('Refresh token');
+    cy.location("pathname").should("eq", "/");
+    cy.findByText(member.lastName, {exact: false});
+
+    cy.visit("/settings");
+    cy.findByRole("button", {name: /Refresh token/});
   });
 });
