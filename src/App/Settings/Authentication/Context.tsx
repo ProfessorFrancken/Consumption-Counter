a6e6bdb6c46e391ee92f3a2f35ac233ab81ab91d
Mark Redeman <markredeman@gmail.com>
2021-02-01 19:16:23 +0100

    Show AuthenticationForm when user is not authenticated

--- src/App/Settings/Authentication/Context.tsx
+++ src/App/Settings/Authentication/Context.tsx
@@ -55,16 +55,16 @@ export const AuthenticationProvider: React.FC<{token?: string}> = ({
     authenticate: login.mutate,
   };
 
-  if (false && !value.token) {
+  if (!value.token) {
     return (
       <AuthenticationContext.Provider value={value} {...props}>
         <div className="d-flex align-items-center justify-content-center h-100">
           <div className="d-flex align-items-center">
-            <div className="px-4">
+            <div className="mr-5">
               <img
                 src={logo}
                 className="franckenLogo img-fluid"
-                width={200}
+                width={225}
                 alt="Logo of T.F.V. 'Professor Francken'"
               />
             </div>
