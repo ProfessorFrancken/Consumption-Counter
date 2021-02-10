d48325d94ec1c3b0925804f8f75082a5746e042b
Mark Redeman <markredeman@gmail.com>
2021-02-10 19:17:23 +0100

    Add test for Present

---
+++ src/App/Present/index.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {render, fireEvent} from "test-utils";
+import PresentScreen from "./index";
+import moxios from "moxios";
+import {history} from "./../../../Setup/store";
+
+describe("Present screen", () => {
+  it("Shows members that are present in the members room", async () => {
+    moxios.install();
+    moxios.stubRequest(`https://borrelcie.vodka/present/data.php`, {
+      response: ["Mark"],
+    });
+    const {findByRole, getByText, getByRole} = render(<PresentScreen />, {
+      storeState: {members: [{id: 1403, fullname: "John Snow", cosmetics: {}}]},
+    });
+
+    expect(getByText("Sponsored by", {exact: false})).toBeInTheDocument();
+    expect(await findByRole("button", {name: /John Snow/})).toBeInTheDocument();
+  });
+});
