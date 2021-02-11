2150c1ac358d54976f55ef06125a25dc6a7a2041
Mark Redeman <markredeman@gmail.com>
2021-02-11 17:51:39 +0100

    Remove memberInRangeSelector

--- src/selectors.ts
+++ src/selectors.ts
@@ -1,5 +1,3 @@
 export const rangesSelector = (state: any) => state.surnameRanges.ranges;
 export const queuedOrderSelector = (state: any) => state.queuedOrder;
 export const queuedOrdersSelector = (state: any) => state.queuedOrders;
-export const membersInRangeSelector = (state: any, {page = 0}) =>
-  state.surnameRanges.ranges[page].members;
