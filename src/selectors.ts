ea52ddc0851bb1ccace81f4134b383d61b918e1f
Mark Redeman <markredeman@gmail.com>
2021-02-13 13:55:02 +0100

    Remove unused selectors

--- src/selectors.ts
+++
@@ -1,3 +0,0 @@
-export const rangesSelector = (state: any) => state.surnameRanges.ranges;
-export const queuedOrderSelector = (state: any) => state.queuedOrder;
-export const queuedOrdersSelector = (state: any) => state.queuedOrders;
