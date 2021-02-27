29f50ebc2e4a37db91df4ae855dbb1e3617448f1
Mark Redeman <markredeman@gmail.com>
2021-02-27 14:01:23 +0100

    Remove menuItems
    We're now being more explicit about menu items. So it's no longer needed
    to make this dynamic.
    
    If we want to have some kind of plugin support we may want to extract
    the menuItems generated in AppContainer to a separate context but for
    now this is not needed.

--- src/Setup/reducers.ts
+++ src/Setup/reducers.ts
@@ -1,14 +1,13 @@
 import {combineReducers} from "redux";
 import {connectRouter} from "connected-react-router";
 
-import {loading, transactions, recentBuyers, menuItems, statistics} from "../reducer";
+import {loading, transactions, recentBuyers, statistics} from "../reducer";
 
 const makeApplicationReducer = (history: any) => {
   return combineReducers({
     loading,
     transactions,
     recentBuyers,
-    menuItems,
     statistics,
     router: connectRouter(history),
   });
