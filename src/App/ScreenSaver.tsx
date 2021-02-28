d005cbb7132017768069d23aff7554631b8f388d
Mark Redeman <markredeman@gmail.com>
2021-02-28 11:09:15 +0100

    Refactor screensaver to function component

---
+++ src/App/ScreenSaver.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {useHistory} from "react-router";
+
+type Props = {
+  goHome: () => void;
+  goToScreenSaver: () => void;
+};
+
+export const SCREEN_SAVER_TIMEOUT = 30000;
+
+const ScreenSaver: React.FC<Props> = ({goHome, goToScreenSaver}) => {
+  const history = useHistory();
+  const {listen, location} = history;
+
+  const [pathname, setPathname] = React.useState(location.pathname);
+  React.useEffect(() => {
+    return listen((location) => {
+      setPathname(location.pathname);
+    });
+  }, [listen, setPathname]);
+
+  React.useEffect(() => {
+    if (pathname === "/") {
+      const screenSaver = setTimeout(() => {
+        goToScreenSaver();
+      }, 2 * SCREEN_SAVER_TIMEOUT);
+      return () => clearTimeout(screenSaver);
+    } else {
+      if (pathname !== "/statistics") {
+        const screenSaver = setTimeout(() => {
+          goHome();
+        }, SCREEN_SAVER_TIMEOUT);
+        return () => clearTimeout(screenSaver);
+      }
+    }
+  }, [pathname, goToScreenSaver, goHome]);
+  return null;
+};
+
+export default ScreenSaver;
