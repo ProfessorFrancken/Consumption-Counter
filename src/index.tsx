4ecd730a7209351606371140b4cfe0fac9fa2aef
Mark Redeman <markredeman@gmail.com>
2021-02-28 11:21:45 +0100

    Remove service worker
    We won't need it.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import {render} from "react-dom";
-import registerServiceWorker from "./Setup/registerServiceWorker";
 import Root from "./Root";
 import makeServer from "./server/index";
 
 makeServer();
 
 render(<Root />, document.getElementById("root"));
-
-registerServiceWorker();
