603b1a6a95705b4e4cd96c4f763f5ba4dd614526
Mark Redeman <markredeman@gmail.com>
2023-09-25 22:03:55 +0200

    Fixing things

--- src/Root.test.tsx
+++ src/Root.test.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import {createRoot} from "react-dom/client";
 import ReactDOM from "react-dom";
 import Root from "./Root";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Root />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const root = createRoot(div);
+  root.render(<Root />);
+  root.unmount();
 });
