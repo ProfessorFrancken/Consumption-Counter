601dbc31e5a542beb99229476b909eea9c01de41
Mark Redeman <markredeman@gmail.com>
2023-09-26 22:29:16 +0200

    Use dataloader

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -3,8 +3,14 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import logo from "assets/logo.png";
+import {useCommittees} from "App/Committees/CommitteesContext";
+import {useBoards} from "App/Prominent/BoardsContext";
+import {useMembers} from "App/Members/Context";
+import {useStatistics} from "App/Statistics/StatisticsContext";
+import {useBackgroundFromOrder, useFailedOrders} from "App/QueuedOrdersContext";
+import {useNavigate} from "react-router";
 
-const backgroundFromProduct = (background = null) => {
+const backgroundFromProduct = (background: string | null) => {
   return background === null
     ? {}
     : {
@@ -17,30 +23,85 @@ const backgroundFromProduct = (background = null) => {
 
 const Content = (props: any) => <main className="content">{props.children}</main>;
 
-export const Layout = ({
-  menuItems,
-  goToCompucieScreen,
-  goToProminent,
-  background,
-  goHome,
-  failedOrders,
-  children,
-}: any) => (
-  <div className="wrapper" style={backgroundFromProduct(background)} data-testid="layout">
-    <Header onClick={goToCompucieScreen} failedOrders={failedOrders} />
-    <Sidebar menuItems={menuItems} />
-    <Content>{children}</Content>
-    <Footer />
-    <div className="arrow">
-      <div className="arrowContent">
-        <img
-          src={logo}
-          className="franckenLogo img-fluid"
-          alt="Logo of T.F.V. 'Professor Francken'"
-          onClick={goHome}
-          onDoubleClick={goToProminent}
-        />
+const useMenuItems = () => {
+  const {committeesQuery} = useCommittees();
+  const {boardsQuery} = useBoards();
+  const {membersQuery} = useMembers();
+  const {statisticsQuery} = useStatistics();
+
+  return [
+    {
+      icon: "chess-queen",
+      url: "/prominent",
+      loading: boardsQuery.isLoading,
+      label: "Prominent",
+    },
+    {
+      icon: "home",
+      url: "/",
+      loading: membersQuery.isLoading,
+      label: "Home",
+    },
+    {
+      icon: "clock",
+      url: "/recent",
+      loading: false,
+      label: "Recent",
+    },
+    {
+      icon: "users",
+      url: "/committees",
+      loading: committeesQuery.isLoading,
+      label: "Committees",
+    },
+    {
+      icon: "chart-bar",
+      url: "/statistics",
+      loading: statisticsQuery.isLoading,
+      label: "Statistics",
+    },
+    {
+      icon: "map-marker-alt",
+      url: "/present",
+      loading: false,
+      label: "Present",
+    },
+  ];
+};
+
+export const Layout = ({children}: any) => {
+  const menuItems = useMenuItems();
+  const background = useBackgroundFromOrder();
+  const failedOrders = useFailedOrders();
+  const navigate = useNavigate();
+
+  const goToCompucieScreen = () => navigate("/compucie");
+  const goToProminent = () => navigate("/prominent");
+
+  // Screensaver
+  const goHome = () => navigate("/");
+
+  return (
+    <div
+      className="wrapper"
+      style={backgroundFromProduct(background)}
+      data-testid="layout"
+    >
+      <Header onClick={goToCompucieScreen} failedOrders={failedOrders} />
+      <Sidebar menuItems={menuItems} />
+      <Content>{children}</Content>
+      <Footer />
+      <div className="arrow">
+        <div className="arrowContent">
+          <img
+            src={logo}
+            className="franckenLogo img-fluid"
+            alt="Logo of T.F.V. 'Professor Francken'"
+            onClick={goHome}
+            onDoubleClick={goToProminent}
+          />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
