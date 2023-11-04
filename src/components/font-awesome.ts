135476a8205a19117fe96c04ae265263b7f7e1b4
Mark Redeman <markredeman@gmail.com>
2023-11-04 13:48:44 +0100

    Import fontawesome icons

--- src/components/font-awesome.ts
+++ src/components/font-awesome.ts
@@ -3,25 +3,27 @@ import {library} from "@fortawesome/fontawesome-svg-core";
 import {faGulp} from "@fortawesome/free-brands-svg-icons/faGulp";
 import {faBitcoin} from "@fortawesome/free-brands-svg-icons/faBitcoin";
 import {faSlideshare} from "@fortawesome/free-brands-svg-icons/faSlideshare";
-import {faCheckSquare} from "@fortawesome/free-solid-svg-icons";
-import {faCoffee} from "@fortawesome/free-solid-svg-icons";
-import {faSpinner} from "@fortawesome/free-solid-svg-icons";
-import {faHome} from "@fortawesome/free-solid-svg-icons";
-import {faClock} from "@fortawesome/free-solid-svg-icons";
-import {faUsers} from "@fortawesome/free-solid-svg-icons";
-import {faChartColumn} from "@fortawesome/free-solid-svg-icons";
-import {faChessQueen} from "@fortawesome/free-solid-svg-icons";
-import {faArrowCircleLeft} from "@fortawesome/free-solid-svg-icons";
-import {faCheckCircle} from "@fortawesome/free-solid-svg-icons";
-import {faCogs} from "@fortawesome/free-solid-svg-icons";
-import {faTimesCircle} from "@fortawesome/free-solid-svg-icons";
-import {faShoppingCart} from "@fortawesome/free-solid-svg-icons";
-import {faBeer} from "@fortawesome/free-solid-svg-icons";
-import {faUtensils} from "@fortawesome/free-solid-svg-icons";
-import {faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons";
-import {faSync} from "@fortawesome/free-solid-svg-icons";
-import {faKey} from "@fortawesome/free-solid-svg-icons";
-import {faExclamationTriangle} from "@fortawesome/free-solid-svg-icons";
+import {faCheckSquare} from "@fortawesome/free-solid-svg-icons/faCheckSquare";
+import {faCoffee} from "@fortawesome/free-solid-svg-icons/faCoffee";
+import {faSpinner} from "@fortawesome/free-solid-svg-icons/faSpinner";
+import {faHome} from "@fortawesome/free-solid-svg-icons/faHome";
+import {faClock} from "@fortawesome/free-solid-svg-icons/faClock";
+import {faUsers} from "@fortawesome/free-solid-svg-icons/faUsers";
+import {faChartColumn} from "@fortawesome/free-solid-svg-icons/faChartColumn";
+import {faChessQueen} from "@fortawesome/free-solid-svg-icons/faChessQueen";
+import {faArrowCircleLeft} from "@fortawesome/free-solid-svg-icons/faArrowCircleLeft";
+import {faCheckCircle} from "@fortawesome/free-solid-svg-icons/faCheckCircle";
+import {faCogs} from "@fortawesome/free-solid-svg-icons/faCogs";
+import {faTimesCircle} from "@fortawesome/free-solid-svg-icons/faTimesCircle";
+import {faShoppingCart} from "@fortawesome/free-solid-svg-icons/faShoppingCart";
+import {faBeer} from "@fortawesome/free-solid-svg-icons/faBeer";
+import {faUtensils} from "@fortawesome/free-solid-svg-icons/faUtensils";
+import {faMapMarkerAlt} from "@fortawesome/free-solid-svg-icons/faMapMarkerAlt";
+import {faSync} from "@fortawesome/free-solid-svg-icons/faSync";
+import {faKey} from "@fortawesome/free-solid-svg-icons/faKey";
+import {faExclamationTriangle} from "@fortawesome/free-solid-svg-icons/faExclamationTriangle";
+import {faChevronCircleRight} from "@fortawesome/free-solid-svg-icons/faChevronCircleRight";
+import {faChevronCircleLeft} from "@fortawesome/free-solid-svg-icons/faChevronCircleLeft";
 
 library.add(
   faCoffee,
@@ -46,7 +48,9 @@ library.add(
   faSlideshare,
   faSync,
   faKey,
-  faExclamationTriangle
+  faExclamationTriangle,
+  faChevronCircleRight,
+  faChevronCircleLeft
 );
 
 export default library;
