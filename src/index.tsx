94a0658fbf76340e3f95d6424b175d0f759584b1
Mark Redeman <markredeman@gmail.com>
2023-11-14 12:49:38 +0100

    Make sure msw gets loaded before rendering root

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import {createRoot} from "react-dom/client";
 import {ENVIRONMENT} from "./configuration";
 import Root from "./root";
 
-if (ENVIRONMENT && !(window as any).Cypress) {
-  import("./test-utils/server/msw-browser").then(({worker}) => {
+async function initialize() {
+  if (ENVIRONMENT && !(window as any).Cypress) {
+    const {worker} = await import("./test-utils/server/msw-browser");
     worker.start();
-  });
+  }
 }
 
-const container = document.getElementById("root");
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<Root />);
+initialize().then(() => {
+  const container = document.getElementById("root");
+  const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+  root.render(<Root />);
+});
