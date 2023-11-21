0646245974c87fb4eed778501e60b6a270fdc68a
Mark Redeman <markredeman@gmail.com>
2023-11-21 13:02:06 +0100

    Setup vite legacy pluin so that it works on the old system

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react-swc";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig(({mode}) => {
   // Load env file based on `mode` in the current working directory.
@@ -8,7 +9,20 @@ export default defineConfig(({mode}) => {
 
   return {
     base: "/",
-    plugins: [react()],
+    plugins: [
+      react(),
+      legacy({
+        targets: ["chrome 73"],
+        polyfills: [
+          "es.promise.finally",
+          "es.promise.any",
+          "es/map",
+          "es/set",
+          "es/array",
+        ],
+        modernPolyfills: true,
+      }),
+    ],
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
