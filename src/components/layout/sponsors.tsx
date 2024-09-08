d3d25fd844795e16ec0f9f32f22e40fb85a41535
Mark Redeman <markredeman@gmail.com>
2024-09-08 21:42:07 +0200

    Add Sibren's sponsor button

--- src/components/layout/sponsors.tsx
+++ src/components/layout/sponsors.tsx
@@ -1,6 +1,9 @@
 import {queryOptions, useSuspenseQuery} from "@tanstack/react-query";
 import {Suspense} from "react";
+import sibrenImage from "./../../sibren-wobben.png";
 import api from "./../../api";
+import {useSelectMember} from "../orders-context";
+import {useMembers} from "../../queries/members";
 
 const sponsorsQueryOptions = queryOptions({
   queryKey: ["sponsors"],
@@ -15,9 +18,27 @@ const sponsorsQueryOptions = queryOptions({
 
 const SponsorsList = () => {
   const sponsors = useSuspenseQuery(sponsorsQueryOptions);
+  const selectMember = useSelectMember();
+  const {members} = useMembers();
+  const sibren = members.find((member) => {
+    return member.id === 1911;
+  });
 
   return (
     <>
+      {sibren && (
+        <li className="d-flex align-items-center mx-3">
+          <img
+            src={sibrenImage}
+            alt={"Sibren Wobben sponsor"}
+            className="h-100 py-3 img-fluid"
+            style={{maxWidth: "200px"}}
+            onClick={() => {
+              selectMember(sibren);
+            }}
+          />
+        </li>
+      )}
       {sponsors.data.map(({name, image}, idx) => {
         return (
           <li className="d-flex align-items-center mx-3" key={idx}>
